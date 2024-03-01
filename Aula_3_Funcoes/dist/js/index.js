6c27a9438fdf187077cfd3401fb6923351988664
mathesissi <Matheustheking2@gmail.com>
2024-03-01 19:44:31 -0300

    aula de teste

--- Aula_3_Funcoes/dist/js/index.js
+++ Aula_3_Funcoes/dist/js/index.js
@@ -1,98 +1,39 @@
 "use strict";
-// funcao nomeada
-function adicionar(x, y) {
-    return x + y;
+//Exercício 1
+//Crie uma função que receba uma lista de números como parâmetro e retorne o
+//maior número da lista.
+function listarMaior(...lista) {
+    let num = 0;
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i] > num) {
+            num = lista[i];
+        }
+    }
+    return num;
 }
-let resultado = adicionar(2, 5);
-console.log(adicionar(2, 5), resultado);
-// funcao anonima
-let add = function (x, y) { return (x + y); };
-//Funções com tipos
-function adicionarTipada(x, y) {
-    return x + y;
-}
-function somarTipada(p1, p2) {
-    let p = { x: p1.x + p2.x, y: p1.y + p2.y };
-    return p;
-}
-let ponto_1 = { x: 1, y: 5 };
-let ponto_2 = { x: 10, y: 20 };
-let ponto3 = somar(ponto_1, ponto_2); // retorna {x: 11, y: 25}
-//Parâmetros opcionais e padrões
-function nome(primeiro, ultimo) {
-    if (ultimo) {
-        return `${primeiro} ${ultimo}`;
+console.log("Exercicio 1: " + listarMaior(10, 3, 2, 7, 2, 20, 30));
+//Exercício 2
+//Desenvolva uma função que receba um número como parâmetro e verifique se
+//ele é par ou ímpar. Retorne true se for par e false se for ímpar.
+function verificar(x) {
+    if (x % 2 == 0) {
+        return true;
     }
     else {
-        return primeiro;
+        return false;
     }
 }
-console.log(nome('José', 'Silva')); // retorna 'José Silva'
-nome('José'); // retorna 'José'
-function inicializar(valor = 0) {
-    return valor;
-}
-inicializar(); // retorna 0
-inicializar(10); // retorna 10
-//Parâmetros rest
-//Não confundir com REST, recurso para definição de serviços sobre HTTP
-function concatenar(primeiro, ...ultimos) {
-    return primeiro + ' ' + ultimos.join(' ');
-}
-concatenar('a', 'b', 'c', 'd', 'e'); // retorna 'a b c d e';
-function somar(p1, p2) {
-    if (p1 instanceof Array) {
-        return [p1[0] + p2[0], p1[1] + p2[1]];
+console.log("Exercicio 2: " + verificar(4));
+//Exercício 3
+//Implemente uma função que calcule a média aritmética de um array de números
+//e retorne o resultado. Utilize essa função para calcular a média de diferentes
+//conjuntos de números.
+function mediaAritmetica(...numeros) {
+    let soma = 0;
+    for (let i = 0; i > numeros.length; i++) {
+        soma = numeros[i];
     }
-    else {
-        return { x: p1.x + p2.x, y: p1.y + p2.y };
-    }
+    let resultado = soma / numeros.length;
+    return resultado;
 }
-let ponto1 = { x: 1, y: 5 };
-let ponto2 = { x: 10, y: 20 };
-somar(ponto1, ponto2); // retorna {x: 11, y: 25}
-somar([1, 1], [2, 2]); // retorna [3, 3]
-(operacao) => {
-    switch (calculadora) {
-        case "soma": {
-            return numero1 + numero2;
-            break;
-        }
-        case "subtracao": {
-            return numero1 - numero2;
-            break;
-        }
-        case "divisao": {
-            return numero1 / numero2;
-            break;
-        }
-        case "multiplicacao": {
-            return numero1 * numero2;
-            break;
-        }
-        default: {
-            console.log("FOdeu");
-            break;
-        }
-    }
-};
-console.log(calculadora(5, 3, "soma")); // Saída esperada: 8
-console.log(calculadora(10, 2, "subtracao")); // Saída esperada: 8
-console.log(calculadora(4, 5, "multiplicacao")); // Saída esperada: 20
-console.log(calculadora(10, 2, "divisao")); // Saída esperada: 5
-/*
-Exercício 2: Verificador de Palíndromo
-Crie uma função chamada verificarPalindromo que recebe uma string como parâmetro e retorna verdadeiro se a string for um palíndromo
-(ou seja, se ela é lida da mesma forma da esquerda para a direita e da direita para a esquerda) e falso caso contrário.
-
-Resultado esperado:
-console.log(verificarPalindromo("arara")); // Saída esperada: true
-console.log(verificarPalindromo("reviver")); // Saída esperada: true
-console.log(verificarPalindromo("banana")); // Saída esperada: false
-console.log(verificarPalindromo("reconhecer")); // Saída esperada: true
-
-Dica:
-let frase: string = "Ana"
-const fraseInverso = frase.split('').reverse().join('');
-
-*/ 
+console.log("exercicio 3:" + mediaAritmetica(1, 2, 3, 4, 5));
--- Aula_3_Funcoes/src/index.ts
+++ Aula_3_Funcoes/src/index.ts
@@ -1,150 +1,27 @@
-// funcao nomeada
-function adicionar(x:number, y:number) {
-    return x + y;
+//Exercício 1
+//Crie uma função que receba uma lista de números como parâmetro e retorne o
+//maior número da lista.
+function listarMaior (...lista:number[]){
+    let num=0;
+    for(let i=0; i<lista.length; i++){
+        if(lista[i]>num){
+            num=lista[i];
+        }
+    }
+    return num;
 }
-let resultado = adicionar(2,5)
-console.log(adicionar(2,5), resultado)
+console.log("Exercicio 1: " + listarMaior(10,3,2,7,2,20,30));
 
-// funcao anonima
-let add = function(x:number, y:number) { return (x + y); }
-
-
-//Funções com tipos
-function adicionarTipada(x: number, y: number): number {
-    return x + y;
-}
-
-
-function somarTipada(p1: {x: number, y: number}, p2: {x: number, y: number}) : {x: number, y: number} {
-    let p = {x: p1.x + p2.x, y: p1.y + p2.y};
-    return p;
-}
-
-let ponto_1 = {x: 1, y: 5};
-let ponto_2 = {x: 10, y: 20};
-
-let ponto3 = somar(ponto_1, ponto_2); // retorna {x: 11, y: 25}
-
-//Parâmetros opcionais e padrões
-function nome(primeiro: string, ultimo?: string): string {
-    if (ultimo) {
-        return `${primeiro} ${ultimo}`;
-    } else {
-        return primeiro;
+//Exercício 2
+//Desenvolva uma função que receba um número como parâmetro e verifique se
+//ele é par ou ímpar. Retorne true se for par e false se for ímpar.
+function verificar (x: number){
+    if(x % 2 == 0){
+        return true;
+        
+    }
+    else{
+        return false;
     }
 }
-
-console.log(nome('José', 'Silva')); // retorna 'José Silva'
-nome('José'); // retorna 'José'
-
-function inicializar(valor: number = 0) : number {
-    return valor;
-}
-
-inicializar(); // retorna 0
-inicializar(10); // retorna 10
-
-//Parâmetros rest
-//Não confundir com REST, recurso para definição de serviços sobre HTTP
-
-function concatenar(primeiro: string, ...ultimos: string[]): string {
-    return primeiro + ' ' + ultimos.join(' ');
-}
-
-concatenar('a', 'b', 'c', 'd', 'e'); // retorna 'a b c d e';
-
-//Sobrecarga
-function somar(p1: number[], p2x: number[]) : number[];
-function somar(p1: {x: number, y: number}, p2: {x: number, y: number}) : {x: number, y: number};
-function somar(p1: any, p2: any): any {
-    if (p1 instanceof Array) {
-        return [p1[0] + p2[0], p1[1] + p2[1]];
-    } else {
-        return {x: p1.x + p2.x, y: p1.y + p2.y};    
-    }
-}
-
-let ponto1 = {x: 1, y: 5};
-let ponto2 = {x: 10, y: 20};
-
-somar(ponto1, ponto2); // retorna {x: 11, y: 25}
-somar([1, 1], [2, 2]); // retorna [3, 3]
-
-
-
-/*
-Exercício 1: Calculadora Simples
-Crie uma função chamada calculadora que recebe três parâmetros: 
-numero1 (um número), numero2 (um número) e operacao (uma string representando a operação a ser realizada: "soma", "subtracao", "multiplicacao" ou "divisao"). 
-A função deve retornar o resultado da operação entre numero1 e numero2.
-
-Resultado esperado:
-console.log(calculadora(5, 3, "soma")); // Saída esperada: 8
-console.log(calculadora(10, 2, "subtracao")); // Saída esperada: 8
-console.log(calculadora(4, 5, "multiplicacao")); // Saída esperada: 20
-console.log(calculadora(10, 2, "divisao")); // Saída esperada: 5
-
-Dica:
-Estrutura switch-case
-switch(expression) { 
-   case constant-expression1: { 
-      //statements; 
-      break; 
-   } 
-   case constant_expression2: { 
-      //statements; 
-      break; 
-   } 
-   default: { 
-      //statements; 
-      break; 
-   } 
-} 
-
-*/
-function calculadora(numero1: number, numero2: number): number (operacao: string[]): string{
-    switch(calculadora) { 
-        case "soma": { 
-           return numero1 + numero2;
-           break; 
-        } 
-        case "subtracao": { 
-            return numero1 - numero2;
-           break; 
-        } 
-        case "divisao": { 
-            return numero1 / numero2;
-            break; 
-         } 
-        case "multiplicacao": { 
-            return numero1 * numero2;
-            break; 
-         } 
-        default: { 
-            console.log("FOdeu")
-           break; 
-        } 
-     } 
-}
-
-console.log(calculadora(5, 3, "soma")); // Saída esperada: 8
-console.log(calculadora(10, 2, "subtracao")); // Saída esperada: 8
-console.log(calculadora(4, 5, "multiplicacao")); // Saída esperada: 20
-console.log(calculadora(10, 2, "divisao")); // Saída esperada: 5
-
-/*
-Exercício 2: Verificador de Palíndromo
-Crie uma função chamada verificarPalindromo que recebe uma string como parâmetro e retorna verdadeiro se a string for um palíndromo 
-(ou seja, se ela é lida da mesma forma da esquerda para a direita e da direita para a esquerda) e falso caso contrário.
-
-Resultado esperado:
-console.log(verificarPalindromo("arara")); // Saída esperada: true
-console.log(verificarPalindromo("reviver")); // Saída esperada: true
-console.log(verificarPalindromo("banana")); // Saída esperada: false
-console.log(verificarPalindromo("reconhecer")); // Saída esperada: true
-
-Dica:
-let frase: string = "Ana"
-const fraseInverso = frase.split('').reverse().join('');
-
-*/
+console.log("Exercicio 2: " + verificar(4));
